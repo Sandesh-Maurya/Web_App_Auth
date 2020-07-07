31ea431708c3ced7223691102a854afcca9fd80e
Sandesh Maurya <40359412+Sandesh-Maurya@users.noreply.github.com>
2020-07-07 17:13:37 +0530

    Login as Username "user" as and Password as "password"

---
+++ src/main/java/com/MoneyTap/Web_App/WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.MoneyTap.Web_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
